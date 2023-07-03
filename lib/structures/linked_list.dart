5a147ff6de1ee0d75b542f65439228c120de52d2
leandrosconceicao <leandrosantosconceicao@gmail.com>
2023-07-03 16:16:41 -0300

    Implementação de novos metodos e Iterator na estrutura

--- lib/structures/linked_list.dart
+++ lib/structures/linked_list.dart
@@ -11,10 +11,33 @@ class Node<T> {
   }
 }
 
-class LinkedList<E> {
+class _LinkedListIterator<E> implements Iterator<E> {
+  _LinkedListIterator(LinkedList<E> list) : _list = list;
+  final LinkedList<E> _list;
+  Node<E>? _currentNode;
+  bool _firstPass = true;
+  
+  @override
+  E get current => _currentNode!.value;
+  
+  @override
+  bool moveNext() {
+    if (_list.isEmpty) return false;
+    if (_firstPass) {
+      _currentNode = _list.head;
+      _firstPass = false; 
+    } else {
+      _currentNode = _currentNode?.next;
+    }
+    return _currentNode != null;
+  }
+}
+
+class LinkedList<E> extends Iterable<E> {
   Node<E>? head;
   Node<E>? tail;
 
+  @override
   bool get isEmpty => head == null;
 
   void push(E value) {
@@ -56,9 +79,44 @@ class LinkedList<E> {
     return node.next!;
   }
 
+  E? pop() {
+    final value = head?.value;
+    head = head?.next;
+    if (isEmpty) {
+      tail = null;
+    }
+    return value;
+  }
+
+  E? removeLast() {
+    if (head?.next == null) return pop();
+
+    var current = head;
+    while (current!.next != tail) {
+      current = current.next;
+    }
+
+    final value = tail?.value;
+    tail = current;
+    tail?.next = null;
+    return value;
+  }
+
+  E? removeAfter(Node<E> node) {
+    final value = node.next?.value;
+    if (node.next == tail) {
+      tail = node;
+    }
+    node.next = node.next?.next;
+    return value;
+  }
+
   @override
   String toString() {
     if (isEmpty) return "Empty List";
     return head.toString();
   }
+  
+  @override
+  Iterator<E> get iterator => _LinkedListIterator<E>(this);
 }
